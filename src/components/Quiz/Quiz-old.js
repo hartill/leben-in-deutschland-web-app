21873d696cc2ec6f6d3e5a375d242eeae0ba385a
James Hartill <jameshartill@gmail.com>
2017-11-11 19:31:18 +0100

    changed icon

--- src/components/Quiz/Quiz-old.js
+++ src/components/Quiz/Quiz-old.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import './quiz.css';
 import quizQuestions from './../../api/quizQuestions'
+import Lightbox from 'react-image-lightbox';
+import Cookies from 'universal-cookie';
 
 class Quiz extends React.Component {
   constructor(props){
     super(props);
+    this.cookies = new Cookies();
     this.questions = quizQuestions
     this.state = {
       question: {},
       progress: [],
+      incorrect: [],
       showAnswer: false,
-      selectedAnswer: null
+      selectedAnswer: null,
+      completed: false,
+      isOpen: false
     }
     this.generateNextQuestion = this.generateNextQuestion.bind(this);
     this.renderAnswerOptions = this.renderAnswerOptions.bind(this);
     this.onAnswerSelected = this.onAnswerSelected.bind(this);
     this.nextQuestion = this.nextQuestion.bind(this);
+    this.displayAnswers = this.displayAnswers.bind(this);
+    this.restart = this.restart.bind(this);
   }
 
   componentWillMount() {
     this.setState({
       question: this.generateNextQuestion(this.questions),
+      progress: this.cookies.get('progress') || [],
+      incorrect: this.cookies.get('incorrect') || [],
+      showAnswer: false,
+      selectedAnswer: null,
+      completed: false,
+      isOpen: false
     })
   }
 
@@ -34,24 +48,45 @@ class Quiz extends React.Component {
       }
     })
     if (target.value === '1') {
-      while (this.state.progress.includes(questionId)) {
-        questionId += 300
-      }
-      if (questionId <= 3000) {
+      if (this.state.progress.includes(questionId) === false) {
         this.setState(prevState => ({
             progress: [...prevState.progress, questionId]
         }))
       }
+    } else {
+      this.setState(prevState => ({
+          incorrect: [...prevState.incorrect, questionId]
+      }))
     }
+  }
+
+  displayAnswers() {
+    this.setState(prevState => ({
+      showAnswer: true
+    }))
   }
 
   componentDidUpdate(nextProps, nextState) {
     this.state.progress.sort(function (a, b){return a-b})
+    this.state.incorrect.sort(function (a, b){return a-b})
+    const expires = new Date()
+    expires.setTime(expires.getTime()+(365*24*60*60*1000))
+    expires.toUTCString()
+    this.cookies.set('progress', this.state.progress, {expires: expires, path: '/' })
+    this.cookies.set('incorrect', this.state.incorrect, {expires: expires, path: '/' })
+    if ((this.state.progress.length >= 300) && (this.state.progress !== nextState.progress)) {
+      this.setState({
+        completed: true
+      })
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
   }
 
   generateNextQuestion(questions) {
     let correctAnswers = this.state.progress
-    let maxNumber = 3000 - correctAnswers.length
+    let maxNumber = 300 - correctAnswers.length
     let minNumber = 1
     let randNumber = Math.floor((Math.random() * maxNumber) + minNumber);
     for (let i = 0; i < correctAnswers.length; i++) {
@@ -59,14 +94,19 @@ class Quiz extends React.Component {
         randNumber += 1
       }
     }
-    while (randNumber > 300) {
-      randNumber -= 300
-    }
     let randomNumbersIndex = randNumber - 1
     return questions[randomNumbersIndex]
   }
 
-  componentWillReceiveProps(nextProps) {
+  restart() {
+    this.setState({
+        progress: [],
+        incorrect: [],
+        completed: false,
+        showAnswer: false,
+        selectedAnswer: null,
+        isOpen: false
+    })
   }
 
   nextQuestion() {
@@ -122,54 +162,115 @@ class Quiz extends React.Component {
     return output
   }
 
-  render () {
-    let question = this.state.question
-    let userProgress = this.state.progress.length / 3000 * 100
-    let userProgressLabel = Math.ceil(userProgress)
-    let userProgressStyle = {
-      flexBasis: userProgress + '%',
-    }
-    let userProgressLabelStyle = {
-      marginLeft: userProgress + '%',
-    }
+  renderImage(image) {
+    let isOpen = this.state.isOpen
     return (
-      <div className="Container">
-        <div className="QuizContainer">
-          <div className="QuizHeader">
-            <div className="QuestionNumber">
-              {question.id}
-            </div>
-            <div className="QuestionCategory">
-              {question.category}
-            </div>
-            <div className="UserScore">
-              1 x
-            </div>
-          </div>
-          <div className="QuizBodyContainer">
-            <div className="QuizBody">
-              <div className="QuestionText">
-                {question.question}
-              </div>
-              {this.renderAnswerOptions(question, this.showAnswer)}
-            </div>
-            <div className={'NextQuestion'}>
-              <button className={ this.state.showAnswer ===  true ? 'NextQuestionButton Visible' : 'NextQuestionButton Hidden'} onClick={this.nextQuestion} />
-            </div>
-          </div>
-        </div>
-        <div className="UserProgressBarContainer">
-          <div className ="UserProgressBar">
-            <div className ="UserProgressBarCorrect" style={userProgressStyle}></div>
-            <div className ="UserProgressBarIncomplete"></div>
-          </div>
-          <div className ="UserProgressLabelContainer" style={userProgressLabelStyle}>
-            <div className ="UserProgressArrow"></div>
-            <div className ="UserProgressLabel">{userProgressLabel}%</div>
-          </div>
-        </div>
+      <div className="QuestionImage" onClick={() => this.setState({ isOpen: true })}>
+        <p>
+          Bild ansehen
+        </p>
+      {isOpen &&
+      <Lightbox
+          mainSrc={image}
+          onCloseRequest={() => this.setState({ isOpen: false })}
+          onMovePrevRequest={() => {}}
+          onMoveNextRequest={() => {}}
+      />
+      }
       </div>
     )
+  }
+
+  render () {
+    let userProgress = this.state.progress.length
+    let userProgressPercent = 100 - (userProgress / 300 * 100)
+    let userProgressStyle = {
+      right: userProgressPercent + '%',
+    }
+    if (this.state.completed === false) {
+      let question = this.state.question
+      let questionCountStyle = 'yellowCount'
+      let incorrectCount  = this.state.incorrect.reduce(function(n, val) {
+          return n + (val === question.id)
+      }, 0)
+      let correctCount = this.state.progress.reduce(function(n, val) {
+          return n + (val === question.id)
+      }, 0)
+      let totalCount = correctCount - incorrectCount
+      totalCount < 0 ? questionCountStyle = 'redCount' :  null
+      totalCount > 0 ? questionCountStyle = 'greenCount' : null
+      let image = this.state.question.image !== undefined ? require(`./../../static/images/${question.image}`) : null
+      return (
+        <div className="Container">
+          <div className="QuizContainer">
+            <div className="QuizHeader">
+              <div className="QuestionNumber">
+                {question.id}
+              </div>
+              <div className='QuestionCategory'>
+                {question.category}
+              </div>
+              <div className={'QuestionCount' + ' ' + questionCountStyle}>
+                {totalCount}
+              </div>
+            </div>
+            <div className="QuizBodyContainer">
+              <div className="QuizBody">
+                <div className="QuestionText">
+                  {question.question}
+                </div>
+                {question.image !== undefined ? this.renderImage(image) : null}
+                {this.renderAnswerOptions(question, this.showAnswer)}
+              </div>
+            </div>
+            <div className='NextQuestion'>
+              <div className="UserScore">
+                <div className='UserProgress' style={userProgressStyle}></div>
+                <div className='Score'>
+                  {userProgress} / 300
+                </div>
+              </div>
+              <button className={ this.state.showAnswer ===  true ? 'NextQuestionButton Visible' : 'NextQuestionButton Hidden'} onClick={this.nextQuestion} >
+                <img src ={require("./../../static/images/next-qu-icon-01.png")} alt='next-question' />
+              </button>
+              <button className={ this.state.showAnswer ===  false ? 'NextQuestionButton Visible' : 'NextQuestionButton Hidden'} onClick={this.displayAnswers} >
+                <p>Ich wei&szlig; nicht</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div className="Container">
+          <div className="QuizContainer">
+            <div className="QuizHeader">
+              <div className="QuestionCategory">
+                Alle 300 richtig!
+              </div>
+            </div>
+            <div className="QuizBodyContainer">
+              <div className="QuizBody">
+                <div className="ResultBarBlack"></div>
+                <div className="ResultBarRed"></div>
+                <div className="ResultBarYellow"></div>
+              </div>
+            </div>
+            <div className='NextQuestion'>
+              <div className="UserScore">
+                <div className='UserProgress' style={userProgressStyle}></div>
+                <div className='Score'>
+                  {userProgress} / 300
+                </div>
+              </div>
+              <button className='NextQuestionButton' onClick={this.restart} >
+                <p>Ne&uuml;start?</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+    }
   }
 }
 
